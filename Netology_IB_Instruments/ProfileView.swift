14a3ae09842cf13eeec3de75f209e9421c158aee
Pavlova Tatyana <etaniks@mail.ru>
2022-02-02 14:35:28 +0300

    ProfileView.xib added and modified

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Ilya on 02.02.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myLabelName: UILabel!
+    @IBOutlet weak var myLabelBirthday: UILabel!
+    @IBOutlet weak var myLabelCity: UILabel!
+    @IBOutlet weak var myTextView: UITextView!
+}
