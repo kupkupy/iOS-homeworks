ed7c4be692e705a8fe887e9748981ce5f7cdc1cb
Pavlova Tatyana <etaniks@mail.ru>
2022-02-02 17:58:53 +0300

    Image added, ProfileViewController.swift and Profile.storyboard added

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -5,14 +5,15 @@
 //  Created by Ilya on 02.02.2022.
 //
 
-import Foundation
 import UIKit
 
 class ProfileView: UIView {
-    
+
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myLabelName: UILabel!
     @IBOutlet weak var myLabelBirthday: UILabel!
     @IBOutlet weak var myLabelCity: UILabel!
     @IBOutlet weak var myTextView: UITextView!
+    
+    
 }
